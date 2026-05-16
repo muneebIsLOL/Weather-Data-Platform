1e703893bf682fa0201954791438b9012e2c2557
muneeb <qastconsultants@gmail.com>
2026-05-16 20:44:15 +0500

    add cleaning logic for units

--- backend/src/ELT/transform.py
+++ backend/src/ELT/transform.py
@@ -3,6 +3,7 @@ import pandas as pd
 import datetime
 import numpy as np
 
+
 def load_raw(schema_reference: dict, engine):
     data = {}
     for key, value in schema_reference.items():
@@ -10,17 +11,36 @@ def load_raw(schema_reference: dict, engine):
 
     return data
 
+
+schema_ref = {
+    "current_conditions": "weather_current_raw",
+    "hourly_conditions": "weather_hourly_raw",
+    "daily_conditions": "weather_daily_raw",
+    "metadata": "weather_metadata",
+    "units": "weather_units",
+}
+
+data = load_raw(schema_ref, engine)
+
 def clean_data(data: dict):
     for value in data.values():
-        value["id"] = value["id"].ffill()
-        if "time" in value.columns:
-            value["time"] = value["time"].fillna(datetime.datetime.now())
-            value["time"] = pd.to_datetime(value["time"])
+        if isinstance(value, pd.DataFrame):
+            value["id"] = value["id"].ffill()
+            if "time" in value.columns:
+                value["time"] = value["time"].fillna(datetime.datetime.now())
+                value["time"] = pd.to_datetime(value["time"])
 
     current_conditions = data["current_conditions"]
     hourly_conditions = data["hourly_conditions"]
     daily_conditions = data["daily_conditions"]
-    metadata = data["weather_metadata"]
+    metadata = data["metadata"]
+    units = data["units"]
+    current_units = pd.json_normalize(units["current_units"])
+    hourly_units = pd.json_normalize(units["hourly_units"])
+    daily_units = pd.json_normalize(units["daily_units"])
+
+    for units in [current_units, hourly_units, daily_units]:
+        units = units.ffill()
 
     hourly_conditions["apparent_temperature"] = hourly_conditions[
         "apparent_temperature"
@@ -57,11 +77,18 @@ def clean_data(data: dict):
     for col in ["extract_time_ms", "elevation"]:
         metadata[col] = metadata[col].fillna(metadata[col].mean())
 
-    metadata["utc_offset_secs"] = metadata["utc_offset_secs"].fillna(
-        metadata["fetched_at"].dt.utcoffset().dt.total_seconds()
-    )
+    metadata["utc_offset_secs"] = metadata["utc_offset_secs"].ffill()
+
+    del data["units"]
+
+    data["current_units"] = current_units
+    data["hourly_units"] = hourly_units
+    data["daily_units"] = daily_units
 
     return data
+
+
+print(clean_data(data))
 
 
 def column_mapping(data: dict):
@@ -120,25 +147,33 @@ def column_mapping(data: dict):
 
     for conditions in [current_conditions, hourly_conditions]:
         conditions["feels_like"] = (
-            conditions["weather_code"].map(WEATHER_CODES).fillna("NS").astype("category")
+            conditions["weather_code"]
+            .map(WEATHER_CODES)
+            .fillna("UNKNOWN_CODE")
+            .astype("category")
         )
     idx = (((hourly_conditions["wind_direction_10m"] + 11.25) // 22.5) % 16).astype(int)
     hourly_conditions["wind_direction_cardinal"] = idx.apply(
         lambda x: cardinal_directions[x]
-    ).astype("category")
-    
+    ).astype("category").fillna("UNKNOWN")
+
     return data
 
+
 def normalize(data: dict):
-    for value in [data["current_conditions"], data["hourly_conditions"], data["daily_conditions"]]:
+    for value in [
+        data["current_conditions"],
+        data["hourly_conditions"],
+        data["daily_conditions"],
+    ]:
         value["id"] = value["id"].astype("int8")
 
         if "time" in value.columns:
             value["time"] = pd.to_datetime(value["time"])
-        
+
         elif "fetched_at" in value.columns:
             value["fetched_at"] = pd.to_datetime(value["fetched_at"])
-        
+
         for col in value.loc[:, value.columns.str.contains("temperature")].columns:
             value[col] = value[col].astype(float)
 
@@ -148,8 +183,6 @@ def normalize(data: dict):
     for value in [current_conditions, hourly_conditions]:
         value["relative_humidity_2m"] = value["relative_humidity_2m"].astype(np.float16)
         value["weather_code"] = value["weather_code"].astype(int)
-        value["wind_speed_10m"] = value["wind_speed_10m"].astype(np.float16)
-    
+        value["wind_speed_10m"] = value["wind_speed_10m"].astype(np.float16)        
+
     return data
-
-
