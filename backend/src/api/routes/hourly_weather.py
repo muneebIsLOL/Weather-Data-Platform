b7058f68028ff4e70a83bb6db375e666ffa9fd41
muneeb <qastconsultants@gmail.com>
2026-05-23 13:00:23 +0500

    create api endpoint for hourly weather

---
+++ backend/src/api/routes/hourly_weather.py
@@ -0,0 +1,17 @@
+from src.api.services.weather_service import get_hourly
+from src.api.models.hourly_conditions import HourlyConditionsResponse
+from fastapi import APIRouter
+from typing import List
+
+
+router = APIRouter()
+
+@router.get("/hourly_weather", response_model=List[HourlyConditionsResponse])
+def current_weather():
+    try:
+        response = get_hourly()
+        return response
+
+    except Exception as e:
+        print("Pydantic Model Validation Error at /hourly_weather")
+        raise e
