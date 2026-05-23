456c53f3d4077af6881433952482e61d09b3aad0
muneeb <qastconsultants@gmail.com>
2026-05-23 13:00:31 +0500

    create api endpoint for daily weather

---
+++ backend/src/api/routes/daily_weather.py
@@ -0,0 +1,27 @@
+from src.api.services.weather_service import get_today, get_daily_forecast
+from src.api.models.daily_conditions import DailyConditionsResponse
+from typing import List
+from fastapi import APIRouter
+
+
+router = APIRouter(prefix="/daily")
+
+@router.get("/today", response_model=List[DailyConditionsResponse])
+def today_weather():
+    try:
+        response = get_today()
+        return response
+
+    except Exception as e:
+        print("Pydantic Model Validation Error at /daily_weather")
+        raise e
+
+@router.get("/forecast", response_model=List[DailyConditionsResponse])
+def daily_weather_forecast():
+    try:
+        response = get_daily_forecast()
+        return response
+
+    except Exception as e:
+        print("Pydantic Model Validation Error at /daily_weather")
+        raise e
