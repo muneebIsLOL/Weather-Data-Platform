0efac6af5c98819f21d3c79f6cc9f23d30f366b2
muneeb <qastconsultants@gmail.com>
2026-06-09 20:55:47 +0500

    add a function that capitalizes first letter

---
+++ frontend/src/utils/text.jsx
@@ -0,0 +1,10 @@
+function capitalizeFirstLetter(str) {
+    if (!str) return '';
+
+    return str
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+}
+
+export default capitalizeFirstLetter;
