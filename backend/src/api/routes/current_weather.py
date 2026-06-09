23a153eed6561b9d6907393d32cf8d978f3764b0
muneeb <qastconsultants@gmail.com>
2026-06-09 20:44:09 +0500

    denest response model from list for consistency and flexibility

--- backend/src/api/routes/current_weather.py
+++ backend/src/api/routes/current_weather.py
@@ -6,7 +6,7 @@ from typing import List
 
 router = APIRouter()
 
-@router.get("/current_weather", response_model=List[CurrentConditionsResponse])
+@router.get("/current_weather", response_model=CurrentConditionsResponse)
 def current_weather():
     try:
         response = get_current()
@@ -15,3 +15,4 @@ def current_weather():
     except Exception as e:
         print("Pydantic Model Validation Error at /current_weather")
         raise e
+    
