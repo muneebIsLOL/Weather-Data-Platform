0a09d74e546f20851e6b80be923792e49cd88769
muneeb <qastconsultants@gmail.com>
2026-06-17 14:03:21 +0500

    replace old db variables with container db variables

--- backend/src/db/postgres.py
+++ backend/src/db/postgres.py
@@ -6,10 +6,12 @@ from urllib.parse import quote_plus
 
 load_dotenv()
 
-USERNAME = os.getenv("USER_NAME")
-PASSWORD = os.getenv("PASSWORD")
+USERNAME = os.getenv("POSTGRES_USER")
+PASSWORD = os.getenv("POSTGRES_PASSWORD")
 PASSWORD = quote_plus(PASSWORD)
+HOST = os.getenv("POSTGRES_HOST", "elt-db")
+PORT = 5432
 
 engine = create_engine(
-    f"postgresql+psycopg2://{USERNAME}:{PASSWORD}@localhost:5432/etl_db"
+    f"postgresql+psycopg2://{USERNAME}:{PASSWORD}@{HOST}:5432/elt_db"
 )
