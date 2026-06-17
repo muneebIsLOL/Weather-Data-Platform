737caca20215242f2e3a63d9603a2a1a80a54b47
muneeb <qastconsultants@gmail.com>
2026-06-17 14:04:28 +0500

    replace old paths with container paths

--- backend/src/ELT/load.py
+++ backend/src/ELT/load.py
@@ -1,11 +1,11 @@
 import pandas as pd
-from src.ELT.utilities.upsert import upsert_df
+from ELT.utilities.upsert import upsert_df
 
 def load_data(engine, transformed_schema_reference: dict):
     data = {}
     try:
         for key, value in transformed_schema_reference.items():
-            path = f"backend/src/ELT/temp/{key}.parquet"
+            path = f"./src/ELT/temp/{key}.parquet"
             data[key] = pd.read_parquet(path)
 
             if "units" in key:
