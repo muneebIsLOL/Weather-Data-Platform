be87e3559610d82221858e0d6c1df9a7405371ea
muneeb <qastconsultants@gmail.com>
2026-07-31 17:34:02 +0500

    chore(dep): update auth dep to match the new .env variable

--- backend/src/api/dependencies/auth.py
+++ backend/src/api/dependencies/auth.py
@@ -5,7 +5,7 @@ import os
 load_dotenv()
 
 def login(token: str = Header(None)):
-    auth_token = os.getenv("AUTH_ACCESS_TOKEN")
+    auth_token = os.getenv("APP_AUTH_ACCESS_TOKEN")
     if token != auth_token:
         raise HTTPException(403, "Invalid Token")
     
