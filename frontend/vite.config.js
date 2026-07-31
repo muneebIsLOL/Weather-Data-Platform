a5291954b81b1761627757c3a78c5cb4ef4b41cc
muneeb <qastconsultants@gmail.com>
2026-07-31 18:01:30 +0500

    chore(cfg): add VITE_HOST_URL .env variables to server.allowedHosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env variables without the VITE_ prefix if needed.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      allowedHosts: [
+        // Ensure this variable is defined in your .env file
+        env.VITE_HOST_URL 
+      ]
+    }
+  }
 })
