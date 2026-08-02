cc094565c3546c4fcaf90f3957d44e0c2c36e7a7
muneeb <qastconsultants@gmail.com>
2026-08-02 13:18:41 +0500

    chore(route): update app to include react router endpoints for different routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { HealthCheck } from "./routes/health/Health.jsx"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/health" element={<HealthCheck />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
