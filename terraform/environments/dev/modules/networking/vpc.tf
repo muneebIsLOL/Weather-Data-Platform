ba51cc16ba14ee9aafaab0c3d343c1efdcc63d2c
muneeb <qastconsultants@gmail.com>
2026-08-07 20:39:30 +0500

    chore(vpc): add flow logging functionality to vpc

--- terraform/environments/dev/modules/networking/vpc.tf
+++ terraform/environments/dev/modules/networking/vpc.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "this" {
   cidr_block           = var.cidr_block
-  instance_tenancy     = "default"
+  instance_tenancy     = "dedicated"
   enable_dns_hostnames = true
   enable_dns_support   = true
 
@@ -24,6 +24,7 @@ resource "aws_subnet" "private" {
 resource "aws_subnet" "public" {
   for_each = var.public_subnets
   vpc_id   = aws_vpc.this.id
+  map_public_ip_on_launch = true
 
   cidr_block        = each.value.cidr_block
   availability_zone = each.value.az
@@ -31,4 +32,12 @@ resource "aws_subnet" "public" {
   tags = {
     Name = "${var.project_name}-${var.environment}-public-${each.key}"
   }
+}
+
+resource "aws_flow_log" "this" {
+  iam_role_arn    = var.external_cloudwatch_log_group_arn
+  log_destination = var.external_flow_log_role_arn
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.this.id
+  depends_on = [var.external_logs_permission_policy_id]
 }
