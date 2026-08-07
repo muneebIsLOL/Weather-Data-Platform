6f80002bc81d5802fcae3bce2f341bcdc16d2fa8
muneeb <qastconsultants@gmail.com>
2026-08-07 20:36:39 +0500

    feat(iac): add iam permissions for vpc resources such as subnets and route tables

---
+++ terraform/environments/dev/iam/vpc.tf
@@ -0,0 +1,92 @@
+resource "aws_iam_role_policy" "this" {
+  name = "vpc-permissions"
+  role = data.aws_iam_session_context.current.issuer_name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Sid    = "AllowVpcCreation"
+        Effect = "Allow"
+
+        Action = [
+          "ec2:CreateVpc"
+        ]
+
+        Resource = "*"
+      },
+
+      {
+        Sid    = "AllowSubnetCreation"
+        Effect = "Allow"
+
+        Action = [
+          "ec2:CreateSubnet*"
+        ]
+
+        Resource = "*"
+      },
+
+      {
+        Sid    = "AllowInternetGatewayManagement"
+        Effect = "Allow"
+
+        Action = [
+          "ec2:CreateInternetGateway",
+          "ec2:AttachInternetGateway"
+        ]
+
+        Resource = "*"
+      },
+
+      {
+        Sid    = "AllowNatGatewayManagement"
+        Effect = "Allow"
+
+        Action = [
+          "ec2:CreateNatGateway",
+          "ec2:AssociateNatGatewayAddress"
+        ]
+
+        Resource = "*"
+      },
+
+      {
+        Sid    = "AllowRouteTableManagement"
+        Effect = "Allow"
+
+        Action = [
+          "ec2:CreateRouteTable",
+          "ec2:CreateRoute",
+          "ec2:ReplaceRoute",
+          "ec2:AssociateRouteTable"
+        ]
+
+        Resource = "*"
+      },
+
+      {
+        Sid    = "AllowResourceTagging"
+        Effect = "Allow"
+
+        Action = [
+          "ec2:CreateTags"
+        ]
+
+        Resource = "*"
+      },
+
+      {
+        Sid    = "AllowEc2Describe"
+        Effect = "Allow"
+
+        Action = [
+          "ec2:Describe*"
+        ]
+
+        Resource = "*"
+      }
+    ]
+  })
+}
