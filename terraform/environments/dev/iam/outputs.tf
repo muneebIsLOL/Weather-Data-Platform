87e65b604b8b90e1a93e4baf40a3cd1ffffd829a
muneeb <qastconsultants@gmail.com>
2026-08-08 16:59:49 +0500

    feat(iam): add an output for cloudwatch logs policy id for OIDC role

--- terraform/environments/dev/iam/outputs.tf
+++ terraform/environments/dev/iam/outputs.tf
@@ -3,12 +3,17 @@ output "flow_log_role_arn" {
   value       = aws_iam_role.flow_log_role.arn
 }
 
-output "logs_permission_policy_id" {
+output "vpc_logs_permission_policy_id" {
   description = "Inline policy for flow log role for vpc in cloudwatch."
-  value       = aws_iam_role_policy.logs_permissions.id
+  value       = aws_iam_role_policy.vpc_logs_permissions.id
 }
 
 output "vpc_iam_permission_policy_id" {
   description = "Vpc permission policy id."
-  value       = aws_iam_role_policy.cloudwatch_policy.id
+  value       = aws_iam_role_policy.this.id
+}
+
+output "terraform_cloudwatch_policy_id" {
+  description = "Cloudwatch policy id for OIDC github role."
+  value       = aws_iam_role_policy.terraform_cloudwatch_policy.id
 }
