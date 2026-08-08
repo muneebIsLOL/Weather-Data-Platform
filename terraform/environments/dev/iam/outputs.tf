22775483eafe993ef3c065d7b39daf92c7cb40c0
muneeb <qastconsultants@gmail.com>
2026-08-08 09:09:25 +0500

    feat(iac): add output value of vpc policy id to pass into other modules and resources

--- terraform/environments/dev/iam/outputs.tf
+++ terraform/environments/dev/iam/outputs.tf
@@ -7,3 +7,8 @@ output "logs_permission_policy_id" {
   description = "Inline policy for flow log role for vpc in cloudwatch."
   value       = aws_iam_role_policy.logs_permissions.id
 }
+
+output "vpc_iam_permission_policy_id" {
+  description = "Vpc permission policy id."
+  value = aws_iam_role_policy.this.id
+}
