931c95d3de0907be6a4924c45d2a17417906af24
muneeb <qastconsultants@gmail.com>
2026-08-08 07:03:05 +0500

    feat(iam): add variables for inputs directly from main.tf

---
+++ terraform/environments/dev/iam/variables.tf
@@ -0,0 +1,9 @@
+variable "account_id" {
+  type = string
+  description = "The 12 digit unique AWS account id."
+}
+
+variable "current_role_arn" {
+  type = string
+  description = "The ARN attribute of the current OIDC role."
+}
