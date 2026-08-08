e75831103b6d020924e4d858c8f30d70493e3b81
muneeb <qastconsultants@gmail.com>
2026-08-08 17:49:07 +0500

    chore(iac): remove unnecessary outputs to make the infrastructure less complex

--- terraform/environments/dev/iam/outputs.tf
+++ terraform/environments/dev/iam/outputs.tf
@@ -2,18 +2,3 @@ output "flow_log_role_arn" {
   description = "The authorization badge ARN for the VPC Flow Logs service."
   value       = aws_iam_role.flow_log_role.arn
 }
-
-output "vpc_logs_permission_policy_id" {
-  description = "Inline policy for flow log role for vpc in cloudwatch."
-  value       = aws_iam_role_policy.vpc_logs_permissions.id
-}
-
-output "vpc_iam_permission_policy_id" {
-  description = "Vpc permission policy id."
-  value       = aws_iam_role_policy.this.id
-}
-
-output "terraform_cloudwatch_policy_id" {
-  description = "Cloudwatch policy id for OIDC github role."
-  value       = aws_iam_role_policy.terraform_cloudwatch_policy.id
-}
