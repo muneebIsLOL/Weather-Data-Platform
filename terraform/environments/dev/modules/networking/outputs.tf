cf5b83b4f31e7d3e69a7ceeb2979747bd3f666b4
muneeb <qastconsultants@gmail.com>
2026-08-10 18:59:51 +0500

    chore(iac): add outputs for db related actions such as sg ids and subnets

--- terraform/environments/dev/modules/networking/outputs.tf
+++ terraform/environments/dev/modules/networking/outputs.tf
@@ -2,3 +2,11 @@ output "vpc_id" {
   description = "The unique ID of the created VPC"
   value       = aws_vpc.this.id
 }
+
+output "postgres_sg_id" {
+  value = aws_security_group.postgres_sg.id
+}
+
+output "subnet_ids" {
+  value = [for subnet in aws_subnet.private : subnet.id]
+}
