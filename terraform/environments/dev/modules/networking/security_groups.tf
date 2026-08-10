298b3ea9054390dc68286857ff36cfd9c086b0ca
muneeb <qastconsultants@gmail.com>
2026-08-10 19:18:44 +0500

    chore(vpc): update the all cidr block to current vpc cidr block

--- terraform/environments/dev/modules/networking/security_groups.tf
+++ terraform/environments/dev/modules/networking/security_groups.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "postgres_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [aws_vpc.this.cidr_block]
   }
 
   tags = {
