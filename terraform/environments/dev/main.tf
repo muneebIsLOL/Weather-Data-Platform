3fb42300cd0a1f1407f78c0975c79b18f58224ac
muneeb <qastconsultants@gmail.com>
2026-08-14 17:12:19 +0500

    feat(kms): add newly created kms module with output to the s3

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -122,6 +122,14 @@ module "s3" {
   source       = "./modules/s3"
   project_name = var.project_name
   environment  = var.environment
+  kms_key_arn = module.kms.app_key_arn
 
   depends_on = [module.iam]
+}
+
+module "kms" {
+  source = "./modules/kms"
+  current_account_id = data.aws_caller_identity.current
+  project_name = var.project_name
+  environment = var.environment
 }
