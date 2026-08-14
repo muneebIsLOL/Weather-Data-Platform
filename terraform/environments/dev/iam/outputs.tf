c06428f1a96f7044b97635adf38d0160f0fd59ef
muneeb <qastconsultants@gmail.com>
2026-08-14 07:09:44 +0500

    chore(iac): add new outputs for task defs

--- terraform/environments/dev/iam/outputs.tf
+++ terraform/environments/dev/iam/outputs.tf
@@ -2,3 +2,11 @@ output "flow_log_role_arn" {
   description = "The authorization badge ARN for the VPC Flow Logs service."
   value       = aws_iam_role.flow_log_role.arn
 }
+
+output "ecs_task_execution_role_arn" {
+  value = aws_iam_role.ecs_task_execution_role.arn
+}
+
+output "ecs_task_role_arn" {
+  value = aws_iam_role.ecs_task_role.arn
+}
