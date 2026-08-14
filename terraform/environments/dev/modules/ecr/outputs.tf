1ae9e6a8ec788a5e2e5e412576a10ec09f30d99f
muneeb <qastconsultants@gmail.com>
2026-08-14 07:13:44 +0500

    feat(iac): add outputs for each image for task definitions

---
+++ terraform/environments/dev/modules/ecr/outputs.tf
@@ -0,0 +1,26 @@
+data "aws_ecr_image" "backend" {
+  repository_name = aws_ecr_repository.backend.name
+  image_tag       = "1.0.0"
+}
+
+output "backend_image" {
+  value = "${data.aws_ecr_image.backend.repository_url}@${data.aws_ecr_image.backend.image_digest}"
+}
+
+data "aws_ecr_image" "frontend" {
+  repository_name = aws_ecr_repository.frontend.name
+  image_tag       = "1.0.0"
+}
+
+output "frontend_image" {
+  value = "${data.aws_ecr_image.frontend.repository_url}@${data.aws_ecr_image.frontend.image_digest}"
+}
+
+data "aws_ecr_image" "orchestrator" {
+  repository_name = aws_ecr_repository.orchestrator.name
+  image_tag       = "1.0.0"
+}
+
+output "orchestrator_image" {
+  value = "${data.aws_ecr_image.orchestrator.repository_url}@${data.aws_ecr_image.orchestrator.image_digest}"
+}
