dce1340a3c9b9529f2601f82732c03e64ee5c80f
muneeb <qastconsultants@gmail.com>
2026-08-16 17:50:29 +0500

    chore(cert): add environment file db cert file path handling for local and remote databases

--- backend/src/db/connection.py
+++ backend/src/db/connection.py
@@ -3,23 +3,32 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
+from urllib.parse import quote_plus
 
 
-def get_engine(cert_path: str):
+def get_engine():
     load_dotenv()
 
-    POSTGRES_PASSWORD = os.getenv("APP_POSTGRES_PASSWORD")
+    POSTGRES_PASSWORD = quote_plus(os.getenv("APP_POSTGRES_PASSWORD"))
     POSTGRES_USER = os.getenv("APP_POSTGRES_USER")
     DB_HOST = os.getenv("APP_DB_HOST")
     DB_NAME = os.getenv("APP_DB_NAME")
+    CERT_PATH = os.getenv("APP_DB_CERT_PATH")
+
     try:
-        engine = create_engine(
-            f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{DB_HOST}:5432/{DB_NAME}",
-            connect_args={
+        connect_args = {}
+
+        if CERT_PATH:
+            connect_args = {
                 "sslmode": "verify-full",
-                "sslrootcert": cert_path,
+                "sslrootcert": CERT_PATH,
                 "connect_timeout": 5,
-            },
+            }
+
+        engine = create_engine(
+            f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+            f"@{DB_HOST}:5432/{DB_NAME}",
+            connect_args=connect_args,
             pool_pre_ping=True,
         )
 
@@ -27,11 +36,11 @@ def get_engine(cert_path: str):
             result = connection.execute(text("SELECT 1"))
 
         return engine
-    
+
     except SQLAlchemyError as e:
         print(f"Database error: {e}")
         raise e
-    
+
     except Exception as e:
         print(f"Error {e}")
         raise e
