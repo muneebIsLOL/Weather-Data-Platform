eda027fd50616e34e828a775d98254f5d77af228
muneeb <qastconsultants@gmail.com>
2026-08-17 08:10:04 +0500

    chore(sh): add -d flag to prevent initialization problems

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-docker compose -f docker-compose.app.yml --env-file .env.production up --build
+docker compose -f docker-compose.app.yml --env-file .env.production up --build -d
 
-docker compose -f airflow/docker-compose.airflow.yml --env-file .env.production up --build
+docker compose -f airflow/docker-compose.airflow.yml --env-file .env.production up --build -d
