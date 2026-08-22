f12dac84cf3c59fa971b666c5f0119663c52dd40
muneeb <qastconsultants@gmail.com>
2026-08-22 13:47:16 +0500

    chore(db): add dynamic db name handling for CI pipelines

--- backend/src/db/connection.py
+++ backend/src/db/connection.py
@@ -12,8 +12,10 @@ def get_engine():
     POSTGRES_PASSWORD = quote_plus(os.getenv("APP_POSTGRES_PASSWORD"))
     POSTGRES_USER = os.getenv("APP_POSTGRES_USER")
     DB_HOST = os.getenv("APP_DB_HOST")
-    DB_NAME = os.getenv("APP_DB_NAME")
+    APP_DB_NAME = os.getenv("APP_DB_NAME")
+    TEST_DB = os.getenv("APP_TEST_POSTGRES_DB")
     CERT_PATH = os.getenv("APP_DB_CERT_PATH")
+    CI = os.getenv("CI")
 
     try:
         connect_args = {}
@@ -25,9 +27,11 @@ def get_engine():
                 "connect_timeout": 5,
             }
 
+        db_name = TEST_DB if CI == "true" else APP_DB_NAME
+
         engine = create_engine(
             f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
-            f"@{DB_HOST}:5432/{DB_NAME}",
+            f"@{DB_HOST}:5432/{db_name}",
             connect_args=connect_args,
             pool_pre_ping=True,
         )
