3946fff4b745faa3d057fee8a2185b2c1c8c2351
muneeb <qastconsultants@gmail.com>
2026-08-26 01:08:37 +0500

    feat(ssm): add ssm resource blocks for sharing config and values to other terraform directories

---
+++ terraform/environments/dev/bootstrap/ssm.tf
@@ -0,0 +1,17 @@
+resource "aws_ssm_parameter" "flow_log_role_arn" {
+  name = "${var.project_name}/${var.environment}/iam/flow_log_role_arn"
+  type = string 
+  value = aws_iam_role.flow_log_role.arn
+}
+
+resource "aws_ssm_parameter" "ecs_task_execution_role_arn" {
+  name = "${var.project_name}/${var.environment}/iam/ecs_task_execution_role_arn"
+  type = string 
+  value = aws_iam_role.ecs_task_execution_role.arn
+}
+
+resource "aws_ssm_parameter" "ecs_task_role_arn" {
+  name = "${var.project_name}/${var.environment}/iam/ecs_task_role_arn"
+  type = string 
+  value = aws_iam_role.ecs_task_role.arn
+}
