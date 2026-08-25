c9857a2248b20811c0b786fc28c53bee56b98a2f
muneeb <qastconsultants@gmail.com>
2026-08-26 01:07:48 +0500

    chore(var): add region variable along with existing ones

---
+++ terraform/environments/dev/bootstrap/variables.tf
@@ -0,0 +1,26 @@
+variable "account_id" {
+  type        = string
+  description = "The 12 digit unique AWS account id."
+}
+
+variable "current_role_arn" {
+  type        = string
+  description = "The ARN attribute of the current OIDC role."
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "common_tags" {
+  description = "Common tags applied to all resources."
+  type        = map(string)
+}
+
+variable "region" {
+  type = string
+}
