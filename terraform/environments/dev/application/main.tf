dd2a7cff013064d72fdf050d05ae19dfeec03928
muneeb <qastconsultants@gmail.com>
2026-08-26 17:50:49 +0500

    chore(terraform): add .value attribute at the end of aws_ssm_parameter data block to use its value

--- terraform/environments/dev/application/main.tf
+++ terraform/environments/dev/application/main.tf
@@ -3,12 +3,12 @@ module "ecs_backend_task_def" {
   image                 = data.aws_ecr_image.backend
   project_name          = var.project_name
   environment           = var.environment
-  app_db_host           = data.aws_ssm_parameter.app_db_host
-  app_db_name           = data.aws_ssm_parameter.app_db_name
-  app_postgres_password = data.aws_ssm_parameter.app_postgres_password
-  app_postgres_user     = data.aws_ssm_parameter.app_postgres_user
-  execution_role_arn    = data.aws_ssm_parameter.ecs_task_execution_role_arn
-  task_role_arn         = data.aws_ssm_parameter.ecs_task_role_arn
+  app_db_host           = data.aws_ssm_parameter.app_db_host.value
+  app_db_name           = data.aws_ssm_parameter.app_db_name.value
+  app_postgres_password = data.aws_ssm_parameter.app_postgres_password.value
+  app_postgres_user     = data.aws_ssm_parameter.app_postgres_user.value
+  execution_role_arn    = data.aws_ssm_parameter.ecs_task_execution_role_arn.value
+  task_role_arn         = data.aws_ssm_parameter.ecs_task_role_arn.value
 }
 
 module "ecs_airflow_task_def" {
@@ -16,14 +16,14 @@ module "ecs_airflow_task_def" {
   image                             = data.aws_ecr_image.airflow
   project_name                      = var.project_name
   environment                       = var.environment
-  app_db_host                       = data.aws_ssm_parameter.app_db_host
-  app_db_name                       = data.aws_ssm_parameter.app_db_name
-  app_postgres_password             = data.aws_ssm_parameter.app_postgres_password
-  app_postgres_user                 = data.aws_ssm_parameter.app_postgres_user
-  airflow_db_sqlalchemy_conn_string = data.aws_ssm_parameter.airflow_db_sqlalchemy_conn_string
-  execution_role_arn                = data.aws_ssm_parameter.ecs_task_execution_role_arn
-  task_role_arn                     = data.aws_ssm_parameter.ecs_task_role_arn
-  bucket_name                       = data.aws_ssm_parameter.bucket
+  app_db_host                       = data.aws_ssm_parameter.app_db_host.value
+  app_db_name                       = data.aws_ssm_parameter.app_db_name.value
+  app_postgres_password             = data.aws_ssm_parameter.app_postgres_password.value
+  app_postgres_user                 = data.aws_ssm_parameter.app_postgres_user.value
+  airflow_db_sqlalchemy_conn_string = data.aws_ssm_parameter.airflow_db_sqlalchemy_conn_string.value
+  execution_role_arn                = data.aws_ssm_parameter.ecs_task_execution_role_arn.value
+  task_role_arn                     = data.aws_ssm_parameter.ecs_task_role_arn.value
+  bucket_name                       = data.aws_ssm_parameter.bucket.value
 }
 
 module "ecs_frontend_task_def" {
@@ -31,6 +31,6 @@ module "ecs_frontend_task_def" {
   image              = data.aws_ecr_image.frontend
   project_name       = var.project_name
   environment        = var.environment
-  task_role_arn      = data.aws_ssm_parameter.ecs_task_role_arn
-  execution_role_arn = data.aws_ssm_parameter.ecs_task_execution_role_arn
+  task_role_arn      = data.aws_ssm_parameter.ecs_task_role_arn.value
+  execution_role_arn = data.aws_ssm_parameter.ecs_task_execution_role_arn.value
 }
