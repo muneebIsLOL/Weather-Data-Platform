35ed951022c94e7c484a61dcf820d749d897f955
muneeb <qastconsultants@gmail.com>
2026-08-27 17:52:49 +0500

    refactor(terraform): change the name to point to the correct value

--- terraform/environments/dev/infrastructure/data.tf
+++ terraform/environments/dev/infrastructure/data.tf
@@ -1,3 +1,3 @@
-data "aws_ssm_parameter" "flow_log_role_arn" {
-  name = "${var.project_name}/${var.environment}/iam/flow_log_role_arn"
+data "aws_ssm_parameter" "cloudwatch_role" {
+  name = "${var.project_name}/${var.environment}/iam/cloudwatch_role"
 }
