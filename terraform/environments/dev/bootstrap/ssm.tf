82721621a89f75a26ba00c585cf126954978e6ed
muneeb <qastconsultants@gmail.com>
2026-08-27 18:20:41 +0500

    fix(terraform): format to prevent ci/cd errors

--- terraform/environments/dev/bootstrap/ssm.tf
+++ terraform/environments/dev/bootstrap/ssm.tf
@@ -7,15 +7,15 @@ resource "aws_ssm_parameter" "cloudwatch_role" {
 }
 
 resource "aws_ssm_parameter" "ecs_task_execution_role_arn" {
-  name  = "${var.project_name}/${var.environment}/iam/ecs_task_execution_role_arn"
-  type  = "String"
-  value = aws_iam_role.ecs_task_execution_role.arn
+  name       = "${var.project_name}/${var.environment}/iam/ecs_task_execution_role_arn"
+  type       = "String"
+  value      = aws_iam_role.ecs_task_execution_role.arn
   depends_on = [aws_iam_role_policy.terraform_ssm_policy]
 }
 
 resource "aws_ssm_parameter" "ecs_task_role_arn" {
-  name  = "${var.project_name}/${var.environment}/iam/ecs_task_role_arn"
-  type  = "String"
-  value = aws_iam_role.ecs_task_role.arn
+  name       = "${var.project_name}/${var.environment}/iam/ecs_task_role_arn"
+  type       = "String"
+  value      = aws_iam_role.ecs_task_role.arn
   depends_on = [aws_iam_role_policy.terraform_ssm_policy]
 }
