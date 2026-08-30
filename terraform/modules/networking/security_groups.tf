3472ca88fb31fd242be1d75f453e4fbf1a6c765c
muneeb <qastconsultants@gmail.com>
2026-08-30 15:24:17 +0500

    feat(vpc): add security groups for ALB and ecs services

--- terraform/modules/networking/security_groups.tf
+++ terraform/modules/networking/security_groups.tf
@@ -1,3 +1,7 @@
+data "http" "my_public_ip" {
+  url = "https://ifconfig.me"
+}
+
 resource "aws_security_group" "postgres_sg" {
   name        = "postgres-sg"
   description = "Allow PostgreSQL DB access on standard postgres port 5432"
@@ -14,4 +18,76 @@ resource "aws_security_group" "postgres_sg" {
   tags = {
     Name = "postgres-sg"
   }
+}
+
+resource "aws_security_group" "alb_sg" {
+  name        = "${var.project_name}-${var.environment}-alb-sg"
+  description = "Allows Communication between host machine and services through load balancing"
+
+  vpc_id = aws_vpc.this.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.this.cidr_block]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.my_public_ip.response_body)}/32"]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.my_public_ip.response_body)}/32"]
+  }
+
+  ingress {
+    from_port   = 5173
+    to_port     = 5173
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.my_public_ip.response_body)}/32"]
+  }
+}
+
+resource "aws_security_group" "ecs_services_sg" {
+  name        = "${var.project_name}-${var.environment}-ecs-services-sg"
+  description = "Allow cross communication between services, along with communication between their load balancer"
+
+  vpc_id = aws_vpc.this.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.this.cidr_block]
+  }
+
+  ingress {
+    from_port       = 5173
+    to_port         = 5173
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  ingress {
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  ingress {
+    from_port       = 8000
+    to_port         = 8000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  depends_on = [aws_security_group.alb_sg]
 }
