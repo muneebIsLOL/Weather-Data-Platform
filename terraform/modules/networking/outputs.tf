855518310d19584dad2dabf0978d67ca8d26e274
muneeb <qastconsultants@gmail.com>
2026-08-30 23:20:15 +0500

    chore(vpc): create outputs for subnets and security groups required for ALB in application/

--- terraform/modules/networking/outputs.tf
+++ terraform/modules/networking/outputs.tf
@@ -7,6 +7,18 @@ output "postgres_sg_id" {
   value = aws_security_group.postgres_sg.id
 }
 
-output "subnet_ids" {
+output "private_subnets" {
   value = [for subnet in aws_subnet.private : subnet.id]
+}
+
+output "public_subnets" {
+  value = [for subnet in aws_subnet.public : subnet.id]
+}
+
+output "alb_sg" {
+  value = aws_security_group.alb_sg.id
+}
+
+output "ecs_sg" {
+  value = aws_security_group.ecs_services_sg.id
 }
