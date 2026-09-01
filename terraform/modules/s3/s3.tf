2519df5ad4e7f89f58aa161c2817e8edf2d2c33b
muneeb <qastconsultants@gmail.com>
2026-09-01 07:19:45 +0500

    fix(s3): adjust the naming to meet account-regional naming requriements

--- terraform/modules/s3/s3.tf
+++ terraform/modules/s3/s3.tf
@@ -1,5 +1,8 @@
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
 resource "aws_s3_bucket" "app" {
-  bucket           = "${var.project_name}-${var.environment}-app-bucket"
+  bucket           = "${var.project_name}-${var.environment}-app-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.region}-an"
   bucket_namespace = "account-regional"
 }
 
