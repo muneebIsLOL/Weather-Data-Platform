f081923ebb357440672f761a7507481994acfef1
muneeb <qastconsultants@gmail.com>
2026-09-11 16:36:28 +0500

    refactor(frontend): change the extension from .jsx to .tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.js'
 import { HealthCheck } from "./routes/health/Health.jsx"
 import React from "react";
 import ReactDOM from "react-dom/client";
