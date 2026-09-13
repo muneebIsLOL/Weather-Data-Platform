5ad8b6892037ceab201561f9aabbeaa5c3870902
muneeb <qastconsultants@gmail.com>
2026-09-13 11:19:19 +0500

    refactor(frontend): initialize domNode element within a variable and assign the suitable type to prevent errors

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,20 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.js'
-import { HealthCheck } from "./routes/health/Health.jsx"
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.js';
+import { HealthCheck } from "./routes/health/Health.jsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+const domNode = document.getElementById('root') as HTMLDivElement;
+
+createRoot(domNode).render(
   <StrictMode>
-    {/* <App /> */}
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/health" element={<HealthCheck />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
