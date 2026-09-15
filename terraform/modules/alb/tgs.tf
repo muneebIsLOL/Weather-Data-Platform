5e13f9cee58830b3d47b11e6b7b3c8ec03ec8aa9
muneeb <qastconsultants@gmail.com>
2026-09-15 09:55:55 +0500

    chore(terraform): change the port for frotnend tg to 5173

--- terraform/modules/alb/tgs.tf
+++ terraform/modules/alb/tgs.tf
@@ -1,6 +1,6 @@
 resource "aws_lb_target_group" "frontend_tg" {
   name        = "${var.project_name}-${var.environment}-frontend-tg"
-  port        = 80
+  port        = 5173
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
   target_type = "ip"
