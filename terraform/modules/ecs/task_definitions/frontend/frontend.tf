4b24cdcb8246d1abbfee7b02798cdcf3933b432d
muneeb <qastconsultants@gmail.com>
2026-09-15 09:46:09 +0500

    chore(terraform): add dynamic "APP_AUTH_ACCESS_TOKEN" env variable
    Similar to docker compose application implmentation, the same env variable is added
    to the container definition for frontend. Since, both implementation use the same docker image.

--- terraform/modules/ecs/task_definitions/frontend/frontend.tf
+++ terraform/modules/ecs/task_definitions/frontend/frontend.tf
@@ -23,6 +23,10 @@ resource "aws_ecs_task_definition" "frontend" {
             {
                 "name": "VITE_HOST_URL",
                 "value": "${var.vite_host_url}"
+            },
+            {
+                "name": "API_TOKEN",
+                "value": "${var.api_token}"
             }
         ],
         "environmentFiles": [],
