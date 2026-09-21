50e7c5c059711c3db3e8ef1ab3d09f4e43947eb4
muneeb <qastconsultants@gmail.com>
2026-09-21 15:44:27 +0500

    feat(api): add error handling for different exceptions
    This commit adds except blocks with error messages as HTTPExceptions. In Contrast,
    it throws 500 status a suitable detail text for invalid data and unexpected errors, and,
    404 for empty data for its implication

--- backend/src/api/routes/current_weather.py
+++ backend/src/api/routes/current_weather.py
@@ -3,24 +3,32 @@ from src.api.models.current_conditions import CurrentConditionsResponse
 from fastapi import APIRouter
 from src.db.connection import get_engine
 from pydantic import ValidationError
+from fastapi.exceptions import HTTPException
 
 router = APIRouter()
 
 
 @router.get(
     "/current_weather",
-    response_model=CurrentConditionsResponse,
     tags=["current_weather"],
 )
 def current_weather():
     try:
         engine = get_engine()
         response = get_current(engine)
-        return response
+        return CurrentConditionsResponse(
+            **response if isinstance(response, dict) else response.__dict__
+        )
 
     except ValidationError as e:
-        print("Pydantic Model Validation Error at /current_weather")
-        raise e
-
+        raise HTTPException(
+            status_code=500,
+            detail="Invalid Data Found. Please try again in next 10-15 minutes.",
+        )
     except ValueError as e:
-        return {}
+        raise HTTPException(status_code=404, detail=str(e))
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail="An unexpected error occurred on our servers. Please try your request again later.",
+        )
