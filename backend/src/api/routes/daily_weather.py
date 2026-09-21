eef8d98a54c2acaf9f053f1ddba4e2a36dd7582a
muneeb <qastconsultants@gmail.com>
2026-09-21 15:48:30 +0500

    feat(api): add error handling for different exceptions
    This commit adds except blocks with error messages as HTTPExceptions. In Contrast,
    it throws 500 status a suitable detail text for invalid data and unexpected errors, and,
    404 for empty data for its implication. Moreover, response model argument is gone, and replaced
    with explicit validation before returning the value.

--- backend/src/api/routes/daily_weather.py
+++ backend/src/api/routes/daily_weather.py
@@ -2,39 +2,51 @@ from src.api.services.weather_service import get_today, get_daily_forecast
 from src.api.models.daily_conditions import DailyConditionsResponse
 from typing import List
 from fastapi import APIRouter
-from pydantic import ValidationError
+from pydantic import ValidationError, TypeAdapter
 from src.db.connection import get_engine
+from fastapi.exceptions import HTTPException
 
 router = APIRouter(prefix="/daily")
 
 
-@router.get("/today", response_model=List[DailyConditionsResponse], tags=["today"])
+@router.get("/today", tags=["today"])
 def today_weather():
     try:
         engine = get_engine()
         response = get_today(engine)
-        return response
+        return TypeAdapter(List[DailyConditionsResponse]).validate_python(response)
+
 
     except ValidationError as e:
-        print("Pydantic Model Validation Error at /daily_weather")
-        raise e
-
+        raise HTTPException(
+            status_code=500,
+            detail="Invalid Data Found. Please try again in next 10-15 minutes.",
+        )
     except ValueError as e:
-        return []
+        raise HTTPException(status_code=404, detail=str(e))
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail="An unexpected error occurred on our servers. Please try your request again later.",
+        )
 
 
-@router.get(
-    "/forecast", response_model=List[DailyConditionsResponse], tags=["forecast"]
-)
+@router.get("/forecast", tags=["forecast"])
 def daily_weather_forecast():
     try:
         engine = get_engine()
         response = get_daily_forecast(engine)
-        return response
+        return TypeAdapter(List[DailyConditionsResponse]).validate_python(response)
 
     except ValidationError as e:
-        print("Pydantic Model Validation Error at /daily_weather")
-        raise e
-
+        raise HTTPException(
+            status_code=500,
+            detail="Invalid Data Found. Please try again in next 10-15 minutes.",
+        )
     except ValueError as e:
-        return []
+        raise HTTPException(status_code=404, detail=str(e))
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail="An unexpected error occurred on our servers. Please try your request again later.",
+        )
