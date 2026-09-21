b1cff2ca4e9cd410b35a2df121efae68d9db6304
muneeb <qastconsultants@gmail.com>
2026-09-21 19:37:55 +0500

    feat(tests): add pytests for new error handling implementation in routes and services
    I add 8 new tests in this commit. In contrast, all 4 of my endpoints raise exceptions for 2
    specific types of errors, ValueError & ValidationError. These tests validate those functions exactly.
    Therefore, improving the code.

--- backend/src/tests/test_api.py
+++ backend/src/tests/test_api.py
@@ -1,11 +1,15 @@
-import httpx, os
+import httpx, os, pytest
 from dotenv import load_dotenv
-
+from unittest.mock import patch
+from src.api.routes import daily_weather
+from src.api.routes import hourly_weather
+from src.api.routes import current_weather
 
 load_dotenv()
 token = os.getenv("APP_AUTH_ACCESS_TOKEN")
 
 api = "http://backend-api:8000"
+
 
 def test_docs_endpoint():
     response_code = httpx.get(f"{api}/docs").status_code
@@ -13,7 +17,7 @@ def test_docs_endpoint():
 
 
 def test_current_weather_endpoint(populate_db_data):
-    
+
     response_code = httpx.get(
         f"{api}/current_weather", headers={"token": token}
     ).status_code
@@ -105,3 +109,180 @@ def test_daily_conditions_schema():
 
     adapter = TypeAdapter(List[DailyConditionsResponse])
     assert adapter.validate_python(daily_model)
+
+
+@patch.object(current_weather, "get_current")
+def test_invalid_data_for_current_conditions(mock_func):
+    from src.api.routes.current_weather import current_weather
+    from fastapi import HTTPException
+
+    mock_func.return_value = {
+        "time": "2026-06-19T12:53:16.399Z",
+        "apparent_temp": 0,
+        "temperature": 0,
+        "relative_humidity": 0,
+        "is_day": 0,
+        "wind_speed": 0,
+        "wind_direction": "string",
+        "surface_pressure": 0,
+        "feels_like": 0,
+    }
+
+    with pytest.raises(
+        HTTPException,
+        match="Invalid Data Found. Please try again in next 10-15 minutes.",
+    ) as excinfo:
+        current_weather()
+
+    assert excinfo.value.status_code == 500
+
+
+@patch.object(hourly_weather, "get_hourly")
+def test_invalid_data_for_hourly_weather_conditions(mock_func):
+    from src.api.routes.hourly_weather import hourly_weather
+    from fastapi import HTTPException
+
+    mock_func.return_value = [
+        {
+            "time": "string",
+            "temperature_2m": 0,
+            "relative_humidity_2m": 0,
+            "dew_point_2m": 0,
+            "apparent_temperature": 0,
+            "precipitation_probability": 0,
+            "weather_code": 0,
+            "surface_pressure": 0,
+            "visibility": 0,
+            "wind_speed_10m": 0,
+            "wind_direction_10m": 0,
+            "feels_like": "string",
+            "wind_direction_cardinal": "string",
+            "is_day": "invalid_type",
+        }
+    ]
+
+    with pytest.raises(
+        HTTPException,
+        match="Invalid Data Found. Please try again in next 10-15 minutes.",
+    ) as excinfo:
+        hourly_weather()
+
+    assert excinfo.value.status_code == 500
+
+
+@patch.object(daily_weather, "get_today")
+def test_invalid_data_for_today_weather_conditions(mock_func):
+    from src.api.routes.daily_weather import today_weather
+    from fastapi import HTTPException
+
+    mock_func.return_value = [
+        {
+            "time": "string",
+            "temperature_2m_max": 0,
+            "temperature_2m_min": 0,
+            "sunrise": "string",
+            "sunset": "string",
+            "uv_index_max": "invalid_type",
+        }
+    ]
+
+    with pytest.raises(
+        HTTPException,
+        match="Invalid Data Found. Please try again in next 10-15 minutes.",
+    ) as excinfo:
+        today_weather()
+
+    assert excinfo.value.status_code == 500
+
+
+@patch.object(daily_weather, "get_daily_forecast")
+def test_invalid_data_for_daily_forecast(mock_func):
+    from fastapi import HTTPException
+    from src.api.routes.daily_weather import daily_weather_forecast
+
+    mock_func.return_value = [
+        {
+            "time": "string",
+            "temperature_2m_max": 0,
+            "temperature_2m_min": 0,
+            "sunrise": "string",
+            "sunset": "string",
+            "uv_index_max": "invalid_type",
+        }
+    ]
+
+    with pytest.raises(
+        HTTPException,
+        match="Invalid Data Found. Please try again in next 10-15 minutes.",
+    ) as excinfo:
+        daily_weather_forecast()
+
+    assert excinfo.value.status_code == 500
+
+
+@patch.object(current_weather, "get_current")
+def test_empty_data_for_current_conditions(mock_func):
+    from src.api.routes.current_weather import current_weather
+    from fastapi import HTTPException
+
+    mock_func.side_effect = ValueError(
+        "Data not found. Please try again in next 10-15 minutes."
+    )
+
+    with pytest.raises(
+        HTTPException, match="Data not found. Please try again in next 10-15 minutes."
+    ) as excinfo:
+        current_weather()
+
+    assert excinfo.value.status_code == 404
+
+
+@patch.object(hourly_weather, "get_hourly")
+def test_empty_data_for_hourly_conditions(mock_func):
+    from src.api.routes.hourly_weather import hourly_weather
+    from fastapi import HTTPException
+
+    mock_func.side_effect = ValueError(
+        "Data not found. Please try again in next 10-15 minutes."
+    )
+
+    with pytest.raises(
+        HTTPException, match="Data not found. Please try again in next 10-15 minutes."
+    ) as excinfo:
+        hourly_weather()
+
+    assert excinfo.value.status_code == 404
+
+
+@patch.object(daily_weather, "get_today")
+def test_empty_data_for_today_conditions(mock_func):
+    from src.api.routes.daily_weather import today_weather
+    from fastapi import HTTPException
+
+    mock_func.side_effect = ValueError(
+        "Data not found. Please try again in next 10-15 minutes."
+    )
+
+    with pytest.raises(
+        HTTPException, match="Data not found. Please try again in next 10-15 minutes."
+    ) as excinfo:
+        today_weather()
+
+    assert excinfo.value.status_code == 404
+
+@patch.object(daily_weather, "get_daily_forecast")
+def test_empty_data_for_daily_forecast(mock_func):
+    from src.api.routes.daily_weather import daily_weather_forecast
+    from fastapi import HTTPException
+
+    mock_func.side_effect = ValueError(
+        "Data not found. Please try again in next 10-15 minutes."
+    )
+
+    with pytest.raises(
+        HTTPException, match="Data not found. Please try again in next 10-15 minutes."
+    ) as excinfo:
+        daily_weather_forecast()
+
+    assert excinfo.value.status_code == 404
+
